42205ca5d1bca6a1e897b9c77c2331df11cb13e9
leehaneul623 <dyj03216@naver.com>
2022-07-10 22:34:25 +0900

    새 게시물 입력 받기 기능 구현 시작

--- src/main/java/com/lhn/exam/board/Main.java
+++ src/main/java/com/lhn/exam/board/Main.java
@@ -12,10 +12,20 @@ public class Main {
         while (true){
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
-            System.out.printf("입력된 명령어 : %s\n" , cmd);
 
             if(cmd.equals("exit")){
                 break;
+            }
+            else if(cmd.equals("/usr/article/write")){
+                System.out.println("- 게시물 등록 -");
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+                int id = 1;
+                System.out.printf("%d번 게시물이 입력 되었습니다.\n", id);
+            } else {
+                System.out.printf("입력된 명령어 : %s\n", cmd);
             }
         }
 
