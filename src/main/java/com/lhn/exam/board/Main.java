001c68f1197b0cfa774a40511cb14423e3ce4283
leehaneul623 <dyj03216@naver.com>
2022-07-10 22:43:47 +0900

    등록시마다 게시물번호가 증가하도록

--- src/main/java/com/lhn/exam/board/Main.java
+++ src/main/java/com/lhn/exam/board/Main.java
@@ -9,20 +9,22 @@ public class Main {
         System.out.println("== 게시판 v 0.1 ==");
         System.out.println("== 프로그램 시작 ==");
 
+        int articlesLastId = 0;
+
         while (true){
             System.out.printf("명령) ");
             String cmd = sc.nextLine();
 
             if(cmd.equals("exit")){
                 break;
-            }
-            else if(cmd.equals("/usr/article/write")){
+            } else if(cmd.equals("/usr/article/write")) {
                 System.out.println("- 게시물 등록 -");
                 System.out.printf("제목 : ");
                 String title = sc.nextLine();
                 System.out.printf("내용 : ");
                 String body = sc.nextLine();
-                int id = 1;
+                int id = articlesLastId + 1;
+                articlesLastId = id;
                 System.out.printf("%d번 게시물이 입력 되었습니다.\n", id);
             } else {
                 System.out.printf("입력된 명령어 : %s\n", cmd);
