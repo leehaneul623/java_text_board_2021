9a7b21295a5023e0c57b22987963a551a2abba62
leehaneul623 <dyj03216@naver.com>
2022-07-11 16:02:55 +0900

    /usr/article/list 로 입력하면 게시물리스트 출력, 시작

--- src/main/java/com/lhn/exam/board/Main.java
+++ src/main/java/com/lhn/exam/board/Main.java
@@ -6,7 +6,9 @@ TODO
 - [ ] /usr/article/detail 입력 되면 가장 최근 게시물 정보 노출
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args){
@@ -17,6 +19,13 @@ public class Main {
 
         int articlesLastId = 0;
         Article lastArticle = null;
+        ArrayList<Article> articles = new ArrayList<Article>();
+
+        //테스트 데이터 3개 등록, 시작
+        articles.add(new Article(1, "제목1", "내용1"));
+        articles.add(new Article(2, "제목2", "내용2"));
+        articles.add(new Article(3, "제목3", "내용3"));
+        //테스트 데이터 3개 등록, 끝
 
         while (true){
             System.out.printf("명령) ");
@@ -24,6 +33,16 @@ public class Main {
 
             if(cmd.equals("exit")){
                break;
+            } else if(cmd.equals("/usr/article/list")){
+                System.out.println("- 게시물 리스트 -");
+                System.out.println("---------------------");
+                System.out.println("- 번호 / 제목 -");
+                System.out.println("---------------------");
+
+                for(Article article : articles){
+                    System.out.printf("%d / %s\n", article.id, article.title);
+                }
+
             } else if(cmd.equals("/usr/article/detail")){
 
                 if(lastArticle == null){
