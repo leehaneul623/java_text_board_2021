52e5b235c45b5e867bd4243dad01b5ce507af8f3
leehaneul623 <dyj03216@naver.com>
2022-07-11 19:28:13 +0900

    테스트 게시물 생성 함수 적용

--- src/main/java/com/lhn/exam/board/Main.java
+++ src/main/java/com/lhn/exam/board/Main.java
@@ -11,6 +11,11 @@ import java.util.Scanner;
 
 
 public class Main {
+    static void makeTestData(ArrayList<Article> articles){
+        articles.add(new Article(1, "제목1", "내용1"));
+        articles.add(new Article(2, "제목2", "내용2"));
+        articles.add(new Article(3, "제목3", "내용3"));
+    }
     public static void main(String[] args){
         System.out.println("== 게시판 v 0.1 ==");
         System.out.println("== 프로그램 시작 ==");
@@ -21,11 +26,7 @@ public class Main {
         Article lastArticle = null;
         ArrayList<Article> articles = new ArrayList<Article>();
 
-        //테스트 데이터 3개 등록, 시작
-        articles.add(new Article(1, "제목1", "내용1"));
-        articles.add(new Article(2, "제목2", "내용2"));
-        articles.add(new Article(3, "제목3", "내용3"));
-        //테스트 데이터 3개 등록, 끝
+        makeTestData(articles);
 
         while (true){
             System.out.printf("명령) ");
